df04d7015b9ad648ac4bc1511ec40fdac7399d15
Amar Kisoensingh <amar.kisoensingh@gmail.com>
2019-05-17 11:38:30 +0200

    Ex1 and Ex2

---
+++ Unit1/Unit1.fs
@@ -0,0 +1,46 @@
+/// **Description**
+///
+/// Exercise 1: Implement a function that returns a string containing all numbers from 0 to n. Separate the numbers with a white space.
+///
+/// **Parameters**
+///   * `int`
+///
+/// **Output Type**
+///   * `string`
+///
+let rec allNumber (n: int) =
+  if n <= 0 then
+    string n
+  else
+    allNumber(n - 1) + " " + string n
+
+// let rec allNumber =
+//   fun (n: int) ->
+//     if n <= 0 then
+//       string n
+//     else
+//       allNumber(n - 1) + " " + string n
+
+
+/// **Description**
+/// Exercise 2: Implement a function that returns a string containing all numbers from n to 0. Separate the numbers with a white space.
+/// **Parameters**
+///   * `n` - parameter of type `int`
+///
+/// **Output Type**
+///   * `string`
+///
+/// **Exceptions**
+///
+let rec allNumberRev (n: int) =
+  if n <= 0 then
+    string n
+  else
+    string n + " " + allNumberRev(n - 1)
+
+// let rec allNumberRev =
+//   fun (n: int) ->
+//     if n <= 0 then
+//       string n
+//     else
+//       string n + " " + allNumberRev(n - 1)
