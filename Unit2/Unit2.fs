8e589b3e43148c7932645c04e3ca225f587cf558
Amar Kisoensingh <amar.kisoensingh@gmail.com>
2019-05-24 15:48:01 +0200

    Ex2

--- Unit2/Unit2.fs
+++ Unit2/Unit2.fs
@@ -1,3 +1,10 @@
+// type List <a> = {
+//   kind : "empty"
+// } | {
+//   kind : "cons"
+//   head : a
+//   tail : List <a>
+// }
 
 /// **Description**
 ///
@@ -21,14 +28,39 @@ let rec last =
 
 /// **Description**
 ///
-/// List to test the last function
+/// Exercise 2: Implement a function that creates a list with the elements of l in reverse order.
 ///
 /// **Parameters**
-///
+///   * `l` - parameter of type `List<'a>`
 ///
 /// **Output Type**
-///   * `int list`
+///   * `List<'a>`
 ///
 /// **Exceptions**
 ///
-let lastTest = [1; 2; 3]
+let rec rev (l: List<'a>) : List<'a> =
+  if (l.IsEmpty) then
+    []
+  else
+    (rev l.Tail) @ [l.Head] // With @ you can concatenate two lists -> https://docs.microsoft.com/en-us/dotnet/fsharp/language-reference/lists#operators-for-working-with-lists
+
+
+/// **Description**
+///
+/// Exercise 3: Implement a function that adds all the elements of l2 after those in l1.
+///
+/// **Parameters**
+///   * `l1` - parameter of type `List<'a>`
+///   * `l2` - parameter of type `List<'a>`
+///
+/// **Output Type**
+///   * `List<'a>`
+///
+/// **Exceptions**
+///
+let append (l1: List<'a>) (l2: List<'a>) : List<'a> =
+  l1 @ l2 // With @ you can concatenate two lists -> https://docs.microsoft.com/en-us/dotnet/fsharp/language-reference/lists#operators-for-working-with-lists
+
+let testlist = [1; 2; 3]
+let testl1 = [1; 2; 3; 4; 5]
+let testl2 = [6; 7; 8; 9; 10]
